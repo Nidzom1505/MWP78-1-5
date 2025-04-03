ad219f0b79b9d08d3f1e290a2249bee3fcfb30a3
Nidzom1505 <muhammadnidzomimtiyaz@gmail.com>
2025-04-03 12:21:07 +0700

    belum selesai

--- NO2/index.php
+++ NO2/index.php
@@ -1,5 +1,11 @@
 <?php
+date_default_timezone_set('Asia/Jakarta');
+$tanggal = date('d-m-Y');
+$jam = date('H:i:s');
 
+setcookie('waktu[hari]', $tanggal, time() + 3600);
+setcookie('waktu[jam]', $jam, time() + 3600);
+setcookie('visit_count', isset($_COOKIE['visit_count']) ? $_COOKIE['visit_count'] + 1 : 1, time() + 3600, "/");
 ?>
 
 
@@ -11,6 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
@@ -43,7 +50,7 @@
                     <br>
                     <h4>Already have an account?</h4>
                     <button id="btn-sign-in">
-                        Create account
+                        Sign in
                     </button>
                 </div>
             </div>
@@ -77,16 +84,93 @@
         <div id="modal-sign-up">
             <div class="modal">
                 <div class="modal-top">
-                    <img src="assets/close-svgrepo-com.svg" alt="close">
+                    <img src="assets/close-svgrepo-com.svg" alt="close" id="close">
                     <img src="assets/icon-x.svg" alt="logo">
                 </div>
 
                 <div class="modal-form">
-                    <h3>Create your account</h3>
+                    <h1>Create your account</h1>
+                    <div class="modal-form-input">
+                        <form>
+                            <input type="text" class="form-control" placeholder="Name">
+                            <input type="text" class="form-control" placeholder="Email"><br>
+                            <label>Date of birth</label>
+                            <p>This will not be shown publicly. Confirm your own age, even if this account is for a
+                                business, a pet, or something else.</p>
+
+                            <div class="dob-input">
+                                <select id="day" name="day">
+                                    <option value="" disabled selected>Day</option>
+                                </select>
+
+                                <select id="month" name="month">
+                                    <option value="" disabled selected>Month</option>
+                                    <option value="1">January</option>
+                                    <option value="2">February</option>
+                                    <option value="3">March</option>
+                                    <option value="4">April</option>
+                                    <option value="5">May</option>
+                                    <option value="6">June</option>
+                                    <option value="7">July</option>
+                                    <option value="8">August</option>
+                                    <option value="9">September</option>
+                                    <option value="10">October</option>
+                                    <option value="11">November</option>
+                                    <option value="12">December</option>
+                                </select>
+
+                                <select id="year" name="year">
+                                    <option value="" disabled selected>Year</option>
+                                </select>
+                            </div>
+
+                            <button class="btn-input">Next</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        // Ambil elemen dropdown
+        const daySelect = document.getElementById("day");
+        const yearSelect = document.getElementById("year");
+
+        // Isi dropdown Hari (1-31)
+        for (let i = 1; i <= 31; i++) {
+            let option = document.createElement("option");
+            option.value = i;
+            option.textContent = i;
+            daySelect.appendChild(option);
+        }
+
+        // Isi dropdown Tahun (1900 - Tahun Sekarang)
+        const currentYear = new Date().getFullYear();
+        for (let i = currentYear; i >= 1900; i--) {
+            let option = document.createElement("option");
+            option.value = i;
+            option.textContent = i;
+            yearSelect.appendChild(option);
+        }
+
+        $(function () {
+            $("#btn-create-account").click(function () {
+                $("#modal-sign-up").fadeIn().css("display", "flex");
+            })
+
+            $("#close").click(function () {
+                $("#modal-sign-up").fadeOut();
+            })
+        })
+
+        // localStorage
+        const hari = "<?php echo $tanggal ?>";
+        const jam = "<?php echo $jam ?>";
+
+        localStorage.setItem("hari", hari);
+        localStorage.setItem("jam", jam);
+    </script>
 </body>
 
 </html>
